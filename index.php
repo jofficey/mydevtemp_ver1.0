fb5f17f12302591bff198d2d2cfd589f49bdaf6e
jun <>
2024-05-14 21:13:39 +0900

    ファイル作成

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="format-detection" content="telephone=no">
+
+<title>dev</title>
+
+<meta name="description" content="">
+<meta property="og:title" content="dev">
+<meta property="og:type" content="website">
+<meta property="og:url" content="">
+<meta property="og:image" content="/ogp.jpg">
+<meta property="og:description" content="">
+
+<!-- fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Zen+Kaku+Gothic+New:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+
+<!-- favicon -->
+<link rel="icon" href="/favicon.ico">
+
+<!-- style -->
+<link rel="stylesheet" media="all" href="/assets/css/style.css?<?php echo date('YmdHis'); ?>">
+</head>
+
+<body id="front">
+
+
+<!-- js -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script> -->
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/protonet-jquery.inview/1.1.2/jquery.inview.min.js"></script> -->
+<!-- <script src="https://cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll@15.0.0/dist/smooth-scroll.polyfills.min.js"></script> -->
+<script src="/assets/js/common.js?<?php echo date('YmdHis'); ?>"></script>
+<!-- <script>
+$(function() {
+    $('.slider').slick({
+        fade: true, // fedeオン
+        speed: 1500, // 画像切り替えにかかる時間（ミリ秒）
+        autoplaySpeed: 3000, // 自動スライド切り替え速度
+        arrows: false, // 矢印表示・非表示
+        dots: false, //カレントドット表示
+        autoplay: true, // 自動再生
+        slidesToShow: 1, // スライド表示数
+        slidesToScroll: 1, // スライドする数
+        pauseOnHover: false, //マウスホバーで一時停止
+        infinite: true //ループ
+    });
+});
+</script> -->
+</body>
+</html>
